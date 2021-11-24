62f4177743fc642a1308e514702a508dfdf3731e
Aidan McCarthy <119333306@umail.ucc.ie>
2021-11-24 12:40:07 +0000

    Built simple working GUI

--- src/main/java/com/example/cs3318_assigment3/HelloApplication.java
+++ src/main/java/com/example/cs3318_assigment3/HelloApplication.java
@@ -2,16 +2,18 @@ package com.example.cs3318_assigment3;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
-
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
